8156f92daf7f80cef8035a97b0ae90f82cd2dc50
adibhannachi <adib.hannachi.tn@gmail.com>
2016-04-09 23:57:52 +0100

    Second Commit

---
+++ Client.c
@@ -0,0 +1,111 @@
+/* 
+* (c) 2014-2016 by Adib HANNACHI  
+* @ ENICarthage
+*           ------  Client  ------
+*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#define MAX_PORT 5
+#define PORT_ARRAY_SIZE (MAX_PORT+1)
+#define MAX_MSG 80
+#define MSG_ARRAY_SIZE (MAX_MSG+1)
+#define str(x) # x
+#define xstr(x) str(x)
+int main()
+{	
+	int socketDescriptor, status;
+	unsigned int msgLength;
+	struct addrinfo hints, *servinfo;
+	struct timeval timeVal;
+	fd_set readSet;
+	char msg[MSG_ARRAY_SIZE],serverPort[PORT_ARRAY_SIZE];
+	
+	puts("The server name :");
+	memset(msg, 0, sizeof msg); // Mise à zéro du tampon
+	scanf("%"xstr(MAX_MSG)"s", msg); // nom du serveur
+
+	puts("The port number ");
+	memset(serverPort, 0, sizeof serverPort); // Mise à zéro du tampon
+	scanf("%"xstr(MAX_PORT)"s", serverPort); // numero de port serveur
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	if ((status = getaddrinfo(msg, serverPort, &hints, &servinfo)) != 0) 
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		exit(EXIT_FAILURE);
+	}
+	if ((socketDescriptor = socket(servinfo->ai_family, servinfo->ai_socktype,servinfo->ai_protocol)) == -1) 
+	{
+		perror("socket:");
+		exit(EXIT_FAILURE);
+	}
+
+	puts("\n Welcome to our sports hall.");
+	puts("Your ID Please ");
+	memset(msg,0, sizeof msg);
+	scanf(" %"xstr(MAX_MSG)"[^\n]%*c", msg);
+		if ((msgLength = strlen(msg)) > 0) 
+		{
+			// Envoi de l'identifiant au serveur
+			if (sendto(socketDescriptor, msg, msgLength, 0,servinfo->ai_addr, servinfo->ai_addrlen) == -1) 
+			{
+				perror("sendto:");
+				close(socketDescriptor);
+				exit(EXIT_FAILURE);
+			}
+			// Attente de la réponse pendant une seconde.
+			FD_ZERO(&readSet);
+			FD_SET(socketDescriptor, &readSet);
+			timeVal.tv_sec = 1;
+			timeVal.tv_usec = 0;
+		}
+
+	puts("Number of athletes that you want to register :");
+	memset(msg, 0, sizeof msg); // Mise à zéro du tampon
+	scanf(" %"xstr(MAX_MSG)"[^\n]%*c", msg);
+	// Arrêt lorsque l'utilisateur saisit une ligne ne contenant que la lettre q.
+	while (strcmp(msg, "q")) 
+	{
+		if ((msgLength = strlen(msg)) > 0) 
+		{
+			// Envoi de le nombre insérer au serveur.
+			if (sendto(socketDescriptor, msg, msgLength, 0,servinfo->ai_addr, servinfo->ai_addrlen) == -1) 
+			{
+				perror("sendto:");
+				close(socketDescriptor);
+				exit(EXIT_FAILURE);
+			}
+			// Attente de la réponse pendant une seconde.
+			FD_ZERO(&readSet);
+			FD_SET(socketDescriptor, &readSet);
+			timeVal.tv_sec = 1;
+			timeVal.tv_usec = 0;
+			if (select(socketDescriptor+1, &readSet, NULL, NULL, &timeVal)) 
+			{
+				// Lecture de la ligne modifiée par le serveur.
+				memset(msg, 0, sizeof msg); // Mise à zéro du tampon
+				if (recv(socketDescriptor, msg, sizeof msg, 0) == -1) 
+				{
+					perror("recv:");
+					close(socketDescriptor);
+					exit(EXIT_FAILURE);
+				}
+				printf("Réponse: %s\n", msg);
+			}
+			else 
+			{
+				puts("**The server did not reply in the second.");
+			}
+		}
+		puts("\nTo exit, please insert 'q' only");
+		memset(msg, 0, sizeof msg); // Mise à zéro du tampon
+		scanf(" %"xstr(MAX_MSG)"[^\n]%*c", msg);
+	}
+	close(socketDescriptor);
+	return 0;
+}
---
+++ Serveur.c
@@ -0,0 +1,130 @@
+/* 
+* (c) 2014-2016 by Adib HANNACHI adib.hannachi.tn@gmail.com 
+* @ ENICarthage
+*           ------  Server  ------
+*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+pthread_mutex_t mut;
+int C0=20;
+#define MAX_PORT 5
+#define PORT_ARRAY_SIZE (MAX_PORT+1)
+#define MAX_MSG 80
+#define MSG_ARRAY_SIZE (MAX_MSG+1)
+#define str(x) # x
+#define xstr(x) str(x)
+int main()
+{	
+	FILE* f;	
+	int listenSocket, status, j;
+	unsigned short int msgLength;
+	struct addrinfo hints, *servinfo;
+	struct sockaddr_in clientAddress;
+	socklen_t clientAddressLength = sizeof clientAddress;
+	char msg[MSG_ARRAY_SIZE],msgg[MSG_ARRAY_SIZE],id[MSG_ARRAY_SIZE], listenPort[PORT_ARRAY_SIZE];
+f=fopen("histo.txt", "w");
+close(f);
+                        
+	memset(listenPort, 0, sizeof listenPort); // Mise à zéro du tampon
+	pthread_mutex_init (&mut,NULL);
+	puts("the port number used by Server (on listen ) : ");
+	scanf("%"xstr(MAX_PORT)"s", listenPort);
+	memset(&hints, 0, sizeof hints);
+	
+	hints.ai_family = AF_INET; // IPv4
+	hints.ai_socktype = SOCK_DGRAM; // UDP
+	hints.ai_flags = AI_PASSIVE; // Toutes les adresses disponibles
+
+	if ((status = getaddrinfo(NULL, listenPort, &hints, &servinfo)) != 0) 
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		exit(EXIT_FAILURE);
+	}
+	if ((listenSocket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) 
+	{
+		perror("socket:");
+		exit(EXIT_FAILURE);
+	}
+	if (bind(listenSocket, servinfo->ai_addr, servinfo->ai_addrlen) == -1) 
+	{
+		close(listenSocket);
+		perror("bind:");
+		exit(EXIT_FAILURE);
+	}
+	/*
+	* Libération de la mémoire occupée par les enregistrements
+	*/
+	freeaddrinfo(servinfo);
+	printf("Waiting query on port :%s\n", listenPort);
+	/* Mise à zéro du tampon de façon à connaître le délimiteur de fin de chaîne.
+	*/	
+
+	while (1) 
+	{
+		// Mise à zéro du tampon de façon à connaître le délimiteur de fin de chaîne.
+		memset(id, 0, sizeof id);
+		if (recvfrom(listenSocket, id, sizeof id, 0, (struct sockaddr *) &clientAddress,&clientAddressLength) == -1) 
+		{
+			perror("recvfrom:");
+			close(listenSocket);
+			exit(EXIT_FAILURE);
+		}
+		msgLength = strlen(id);
+		memset(msg, 0, sizeof msg);
+		if (recvfrom(listenSocket, msg, sizeof msg, 0, (struct sockaddr *) &clientAddress,&clientAddressLength) == -1) 
+		{
+			perror("recvfrom:");
+			close(listenSocket);
+			exit(EXIT_FAILURE);
+		}
+		msgLength = strlen(msg);
+		if (msgLength > 0) 
+		{
+			// Affichage de l'adresse IP du client.
+			printf(">> From: %s", inet_ntoa(clientAddress.sin_addr));
+			// Affichage du numéro de port du client.
+			printf("\t Client ID :  %s\n", id);
+			// Affichage de la ligne reçue
+			printf(" Number of registrations requested: %s\n", msg);
+			// Conversion de cette ligne en majuscules.
+			pthread_mutex_lock(&mut);			
+				f=fopen("history.txt", "a+");
+                                j=atoi(msg);
+				if (j<C0)
+				{
+					C0=C0-j;
+					strcpy(msgg,"Inscris");
+					fprintf(f,"%s","Client\t");
+					fprintf(f,"%s \t",id);
+					fprintf(f,"%d",j);
+					fprintf(f,"%s","\t Inscris\n"); 						
+				}
+				else
+				{
+					strcpy(msgg,"NO Inscris");
+					fprintf(f,"%s","Client\t");
+			                fprintf(f,"%s \t",id);
+					fprintf(f,"%d",j);
+					fprintf(f,"%s","\t NO Inscris\n");
+				}
+				fclose(f);
+			pthread_mutex_unlock(&mut);
+			// Renvoi de la ligne convertie au client.
+			 msgLength=strlen(msgg);
+			if (sendto(listenSocket, msgg, msgLength, 0,(struct sockaddr *)&clientAddress,clientAddressLength) == -1) 
+			{
+				perror("sendto:");
+				close(listenSocket);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
